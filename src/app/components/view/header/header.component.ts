d9fe14d245682d8eec06bd64ba4c883f801e8ae8
Tobias Schramek <webtsofficial@gmail.com>
2018-05-03 22:56:57 +0200

    added bootstrap grid, normalize css, hamburgers.css and header style and basic function

--- src/app/components/view/header/header.component.ts
+++ src/app/components/view/header/header.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
 import {faHome} from '@fortawesome/free-solid-svg-icons/faHome';
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons/faShoppingCart';
+import {faFilter} from '@fortawesome/free-solid-svg-icons/faFilter';
+import {faNewspaper} from '@fortawesome/free-solid-svg-icons/faNewspaper';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  faHome = faHome;
+    faHome = faHome;
+    faShopping = faShoppingCart;
+    faFilter = faFilter;
+    faNews = faNewspaper;
+    menuOpen = false;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    toggleMenu() {
+        this.menuOpen = !this.menuOpen;
+    }
 
 }
