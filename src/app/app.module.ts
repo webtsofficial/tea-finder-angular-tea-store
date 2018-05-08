a7a7be2ea863c48850784301aa8ade20f968aad7
Tobias Schramek <webtsofficial@gmail.com>
2018-05-08 09:29:24 +0200

    imported gnu-carousel and hammers and implemented it in site tea-shop-home component
    + added some styles in header for active router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
+// Modules
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
+// Components
 import {AppComponent} from './app.component';
 import {TeaShopHomeComponent} from './components/sites/frontend/tea-shop-home/tea-shop-home.component';
 import {HeaderComponent} from './components/view/header/header.component';
 import {FooterComponent} from './components/view/footer/footer.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {NguCarouselModule} from '@ngu/carousel';
 
 
 @NgModule({
@@ -24,9 +26,12 @@ import {HttpClientModule} from '@angular/common/http';
         FormsModule,
         AppRoutingModule,
         FontAwesomeModule,
-        HttpClientModule
+        HttpClientModule,
+        NguCarouselModule
     ],
-    providers: [],
+    providers: [
+
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/components/sites/frontend/tea-shop-home/tea-shop-home.component.ts
+++ src/app/components/sites/frontend/tea-shop-home/tea-shop-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NguCarousel } from '@ngu/carousel';
+
+import {faCaretLeft} from "@fortawesome/free-solid-svg-icons/faCaretLeft";
+import {faCaretRight} from "@fortawesome/free-solid-svg-icons/faCaretRight";
 
 @Component({
   selector: 'app-tea-shop-home',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeaShopHomeComponent implements OnInit {
 
+  public introCarousel: NguCarousel;
+  faCaretLeft = faCaretLeft;
+  faCaretRight = faCaretRight;
+
   constructor() { }
 
   ngOnInit() {
+      this.introCarousel = {
+          grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+          slide: 1,
+          speed: 400,
+          interval: 4000,
+          point: {
+              visible: true
+          },
+          load: 2,
+          touch: true,
+          loop: true,
+          custom: 'banner'
+      }
+  }
+
+  public introCarouselFunc(event: Event) {
+
   }
 
 }
